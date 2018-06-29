67502c6eef94c30f4713ac74d216df5125b3c073
moritz <moritz.luerig@gmail.com>
2018-06-29 11:38:07 +0200

    improved detection + more settings

--- iso-track.py
+++ iso-track.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created: 2018/06/22
-Last Update: 2018/06/28
-Version 0.1.4
+Last Update: 2018/06/29
+Version 0.1.5
 @author: Moritz Lürig
 """
 
@@ -15,21 +15,31 @@ import cv2
 import os
 
 
+#%% directories
+
+os.chdir("E:\\python1\\iso-track")
+main_dir = "E:\\python1\\iso-track\\example"
+video_in = "E:\\python1\\iso-track\\example\\asellus-sample-1.mp4"
+
 #%% settings
-
-main_dir = "E:\\python1\iso-track\\sandbox"
-video_in = "F:\\4_FID52nurAssel.MP4"
-
-os.chdir(main_dir)
 
 video_name = os.path.splitext(os.path.basename(video_in))[0]
 video_out = video_name + "_out.avi"
 
-start = 0.5 # start frame capture after x minutes | use to cut out handling time in the beginning
-skip = 10 # number of frames to skip (1 = every second frame, 2 = every third frame, ...) | useful when organisms are moving too slow
-roi = True # make video only of region of interest / selected polygon
+# video settings
+
+start = 0 # MINUTES wait before capture after is started | use to cut out handling time in the beginning
+skip = 2 # nFRAMES to skip (1 = capture every second frame, 2 = every third frame, ...) | useful when organisms are moving too slow
+roi = False # make video only of region of interest / selected polygon
+wait = 7.5 # | MINUTES how long can organism sit still
 
 # detection settings
+
+# background-subtractor
+backgr_thresh = 12 # lower = more of organism is detected (+ more noise)
+shadows = True # detect shadows
+min_area=50 # | minimum px area to be included 
+
 
 #blurring
 blur_kern = 10 # for blurring | higher = more coarsely grained 
@@ -37,9 +47,9 @@ blur_thresh = 90 # thresholding after blurring | higher = smaller area
 kern = np.ones((blur_kern,blur_kern))/(blur_kern**2)
 ddepth = -1
 
+#add borders to mask
 dilate_kern = 7
 dilate_iter = 2
-min_area=50
 
 #%% import and define custom functions
 
@@ -66,6 +76,7 @@ poly = PolygonDrawer(video_in)
 poly.run(frame)
 arena_mask = cv2.cvtColor(poly.mask, cv2.COLOR_BGR2GRAY)
 height, width, layers = frame.shape
+
 cap.release()
 
 rx,ry,rwidth,rheight = cv2.boundingRect(poly.points)
@@ -83,18 +94,25 @@ else:
     video_out_writer = cv2.VideoWriter(os.path.join(main_dir,  video_out), fourcc, 25, (width, height), False)
 
 cap = cv2.VideoCapture(video_in)
-fgbg = cv2.createBackgroundSubtractorMOG2(history = 1000, varThreshold = 12, detectShadows = True)
+
+nframes = cap.get(cv2.CAP_PROP_FRAME_COUNT) 
+fps = cap.get(cv2.CAP_PROP_FPS)
+vid_length = str(int(( nframes / fps)/60)).zfill(2) + ":" +str(int((((nframes / fps)/60)-int((nframes / fps)/60))*60)).zfill(2)
+
+history = (wait*60) * (fps / skip)
+fgbg = cv2.createBackgroundSubtractorMOG2(history = int(history), varThreshold = int(backgr_thresh), detectShadows = shadows)
 idx1 = 0
 idx2 = 0
 df = pd.DataFrame()
 ft = pd.DataFrame()
-nframes=cap.get(cv2.CAP_PROP_FRAME_COUNT) 
 
 while(cap.isOpened()):
 
     
     # start video capture (after x minutes)
     ret, frame = cap.read()
+    capture = False
+    
     if ret==False:
         break
 
@@ -102,9 +120,13 @@ while(cap.isOpened()):
     idx2 = idx2 + 1
     if idx2 >= skip:
         idx2 = 0
-    print(idx1)
-
+        
+    mins = str(int((idx1 / fps)/60)).zfill(2)
+    secs = str(int((((idx1 / fps)/60)-int(mins))*60)).zfill(2)
+    
     if idx1 > start * 1800 and idx2 == 0:
+        
+        capture = True
         
         # read arena
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -118,11 +140,7 @@ while(cap.isOpened()):
         ret, thresh = cv2.threshold(mask, blur_thresh, 255, cv2.THRESH_BINARY)
         morph = cv2.dilate(thresh,np.ones((dilate_kern,dilate_kern),np.uint8),iterations = dilate_iter)
         contours = cv2.findContours(morph, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-
         contours = contours[1]
-        
- #       cv2.imshow('overlay', thresh)
-
         
         # filter contours
         contours_good=[]
@@ -159,6 +177,11 @@ while(cap.isOpened()):
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
+    if capture == True:
+        print(mins + ":" + secs + " / " + vid_length + " - " + str(idx1) + " / " + str(int(nframes)) + " - captured" )
+    else:
+        print(mins + ":" + secs + " / " + vid_length + " - " + str(idx1) + " / " + str(int(nframes)))
 
 cap.release()
 video_out_writer.release()
@@ -167,13 +190,13 @@ cv2.destroyAllWindows()
 #%% movement analysis and plotting
 
 # find trajectories - check http://soft-matter.github.io/trackpy/v0.3.0/generated/trackpy.link_df.html for all options
-traj = tp.link_df(df, search_range = 75, memory=250, neighbor_strategy="KDTree", link_strategy="nonrecursive")
-traj_filter = tp.filtering.filter_stubs(traj, threshold=100)
+traj = tp.link_df(df, search_range = 50, memory=300, neighbor_strategy="KDTree", link_strategy="nonrecursive")
+traj_filter = tp.filtering.filter_stubs(traj, threshold=20)
 
 # plot trajectories
 plot = tp.plot_traj(traj, superimpose=arena)
 fig = plot.get_figure()
-fig.savefig(os.path.join(main_dir, video_name + "trajectories.png"), dpi=500)
+fig.savefig(os.path.join(main_dir, video_name + "_trajectories.png"), dpi=300)
 
 # save trajectories to csv
 
